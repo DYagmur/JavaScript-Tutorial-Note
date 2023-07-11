df45d488d7aeb6bfaf7537bbfb1a14f7b96ee477
DYagmur <30656517+DYagmur@users.noreply.github.com>
2023-07-12 02:59:46 +0300

    first js

---
+++ example.js
@@ -0,0 +1,19 @@
+x=1;
+
+
+console.log(x);
+
+
+/*  For loop for javascript; */
+
+
+/* 
+for (starting index, ending index, increment index) {
+ Code to execute for each index element 
+}; */
+
+
+
+for (let i=0, i < )
+
+
